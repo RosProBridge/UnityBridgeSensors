7ead6fa7d449e619aa8c36fe71a5b0c082d1e942
Sherif Nafee <shico.m.nafee@gmail.com>
2024-09-26 17:19:35 +0300

    Update compressed image to get render from gpu and use optimied jpeg encoder

--- Runtime/Tx/CompressedImageTx.cs
+++ Runtime/Tx/CompressedImageTx.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections;
 using sensor_msgs.msg;
+using TurboJpegWrapper;
+using Unity.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace ProBridge.Tx.Sensor
 {
@@ -13,54 +16,94 @@ namespace ProBridge.Tx.Sensor
             jpeg,
             png
         }
-        
-        public RenderTexture renderTexture;
+
         public Format format = Format.jpeg;
 
-
-        private Texture2D tex;
-        private WaitForEndOfFrame frameEnd = new WaitForEndOfFrame();
+        public Camera renderCamera;
+        public int textureWidth = 1024;
+        public int textureHeight = 1024;
+        private RenderTexture renderTexture;
+        private Texture2D texture2D;
         private bool gotFirstFrame = false;
+        private TJCompressor compressor;
+        private bool newFrame = false;
+        private NativeArray<Color32> imageData;
 
         protected override void OnStart()
         {
-            InvokeRepeating("CallSnapShot", 0, sendRate);
+            renderTexture = new RenderTexture(textureWidth, textureHeight, 24, RenderTextureFormat.ARGB32);
+            renderTexture.Create();
+
+            renderCamera.targetTexture = renderTexture;
+
+            texture2D = new Texture2D(textureWidth, textureHeight, TextureFormat.RGBA32, false);
+            compressor = new TJCompressor();
+
+            InvokeRepeating(nameof(RenderLoop), 0, sendRate);
+        }
+
+        void RenderLoop()
+        {
+            renderCamera.Render();
+
+            AsyncGPUReadback.Request(renderTexture, 0, TextureFormat.RGBA32, OnCompleteReadback);
         }
 
         protected override ProBridge.Msg GetMsg(TimeSpan ts)
         {
             data.format = format.ToString();
 
-            if(gotFirstFrame)
+            if (gotFirstFrame)
+            {
                 data.data = format switch
                 {
-                    Format.jpeg => tex.EncodeToJPG(),
-                    Format.png => tex.EncodeToPNG(),
+                    Format.jpeg => compressor.Compress(texture2D.GetRawTextureData(), texture2D.width * 4,
+                        texture2D.width,
+                        texture2D.height, TJPixelFormat.RGBA, TJSubsamplingOption.Chrominance420, 90, TJFlags.None),
+                    Format.png => texture2D.EncodeToPNG(),
                     _ => data.data
                 };
+                newFrame = false;
+            }
             else
-                data.data = new byte[] {0, 0, 0, 255};
+                data.data = new byte[] { 0, 0, 0, 255 };
 
             return base.GetMsg(ts);
         }
 
-
-        private void CallSnapShot()
+        void OnCompleteReadback(AsyncGPUReadbackRequest request)
         {
+            if (request.hasError)
+            {
+                Debug.LogError("GPU readback error detected.");
+                return;
+            }
 
-            StartCoroutine(TakeSnapshot());
-        }
-        
-        public IEnumerator TakeSnapshot()
-        {
-            
-            yield return frameEnd;
-            RenderTexture.active = renderTexture;
-            tex = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
-            tex.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+            if (texture2D == null) return;
+
+            imageData = request.GetData<Color32>();
+
+            texture2D.LoadRawTextureData(imageData);
+            texture2D.Apply();
+
             gotFirstFrame = true;
-            
-            RenderTexture.active = null;
+            newFrame = true;
+
+        }
+
+
+        void OnDestroy()
+        {
+            if (renderTexture != null)
+            {
+                renderTexture.Release();
+            }
+
+            if (texture2D != null)
+            {
+                Destroy(texture2D);
+            }
         }
     }
+    
 }
