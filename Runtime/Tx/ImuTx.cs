994103f57d96442bd53a340870b8289c84d73c65
Sherif Nafee <shico.m.nafee@gmail.com>
2024-10-08 13:52:09 +0300

    Fix imu acceleration

--- Runtime/Tx/ImuTx.cs
+++ Runtime/Tx/ImuTx.cs
@@ -13,30 +13,29 @@ namespace ProBridgeSenors.Tx
         public Rigidbody Body { get; private set; }
         public Vector3 Acceleration { get; private set; }
 
-        private float _lastTime;
         private Vector3 _lastVel;
+        private Vector3 gravityDirection;
+        private float gravityMagnitude;
+        private Transform transform;
 
         protected override void OnStart()
         {
             Body = GetComponent<Rigidbody>();
-            _lastVel = Vector3.zero;
-            _lastTime = Time.time;
+            gravityDirection = Physics.gravity.normalized;
+            gravityMagnitude = Physics.gravity.magnitude;
+            transform = this.transform;
         }
+
+        void FixedUpdate()
+        {
+            Acceleration = (Body.velocity - _lastVel) / Time.deltaTime -
+                           transform.InverseTransformDirection(gravityDirection) * gravityMagnitude;
+            _lastVel = Body.velocity;
+        }
+
 
         protected override ProBridge.ProBridge.Msg GetMsg(TimeSpan ts)
         {
-            var t = Time.time;
-            var dt = t - _lastTime;
-            if (dt <= 0)
-                Debug.LogWarning("Empty delta time");
-            else
-            {
-                var bv = Body.velocity;
-                Acceleration = (bv - _lastVel) / dt;
-                _lastVel = bv;
-                _lastTime = t;
-            }
-
             data.angular_velocity = (Quaternion.Inverse(Body.rotation) * Body.angularVelocity).ToRosAngular();
             data.linear_acceleration = Acceleration.ToRos();
             data.orientation = Body.rotation.ToRos();
