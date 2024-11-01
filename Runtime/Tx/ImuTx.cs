6c913ff8450a51d73f7ad84077da8b454e57e857
Sherif Nafee <shico.m.nafee@gmail.com>
2024-11-01 12:45:43 +0300

    Manually calculate linear and angular for imu

--- Runtime/Tx/ImuTx.cs
+++ Runtime/Tx/ImuTx.cs
@@ -9,54 +9,57 @@ namespace ProBridgeSenors.Tx
     [AddComponentMenu("ProBridge/Tx/sensor_msgs/Imu")]
     public class ImuTx : ProBridgeTxStamped<Imu>
     {
-        public Rigidbody Rigidbody { get; private set; }
-        public ArticulationBody ArticulationBody { get; private set; }
         public Vector3 Acceleration { get; private set; }
 
         public bool isNedImu;
-        private Vector3 _lastVel = new Vector3(0,0,0);
+        private Vector3 _lastVel = new Vector3(0, 0, 0);
         private Vector3 gravityDirection;
         private float gravityMagnitude;
         private Transform initTransform;
 
+        private Vector3 lastPosition;
+        private Quaternion lastRotation;
+        private Vector3 velocity;
+        private Vector3 angularVelocity;
+
 
         protected override void OnStart()
         {
-            Rigidbody = GetComponent<Rigidbody>();
-            if (Rigidbody == null)
-            {
-                ArticulationBody = GetComponent<ArticulationBody>();
-                if (ArticulationBody == null)
-                {
-                    Debug.LogWarning("No Articulation Body or Rigidbody found");
-                    throw new NullReferenceException("No Articulation Body or Rigidbody found");
-                }
-            }
             gravityDirection = Physics.gravity.normalized;
             gravityMagnitude = Physics.gravity.magnitude;
-            initTransform = this.transform;
-
+            initTransform = transform;
+            lastPosition = transform.position;
+            lastRotation = transform.rotation;
         }
+
         void FixedUpdate()
         {
-            var velocity = Rigidbody==null ? ArticulationBody.velocity : Rigidbody.velocity;
+            velocity = (transform.position - lastPosition) / Time.fixedDeltaTime;
+
+            Quaternion deltaRotation = transform.rotation * Quaternion.Inverse(lastRotation);
+            deltaRotation.ToAngleAxis(out var angle, out var axis);
+            angularVelocity = Mathf.Deg2Rad * angle * axis / Time.fixedDeltaTime;
+
+            lastPosition = transform.position;
+            lastRotation = transform.rotation;
+
+
             if (isNedImu)
             {
-                Acceleration = (velocity - _lastVel) / Time.deltaTime - initTransform.InverseTransformDirection(gravityDirection) * gravityMagnitude;
-
+                Acceleration = (velocity - _lastVel) / Time.deltaTime -
+                               initTransform.InverseTransformDirection(gravityDirection) * gravityMagnitude;
             }
             else
             {
                 Acceleration = (velocity - _lastVel) / Time.deltaTime;
             }
+
             _lastVel = velocity;
         }
 
 
         protected override ProBridge.ProBridge.Msg GetMsg(TimeSpan ts)
         {
-            var angularVelocity = Rigidbody==null? ArticulationBody.angularVelocity : Rigidbody.angularVelocity;
-            
             data.angular_velocity = (Quaternion.Inverse(transform.rotation) * angularVelocity).ToRosAngular();
             data.linear_acceleration = Acceleration.ToRos();
             data.orientation = transform.rotation.ToRos();
