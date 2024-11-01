1056a3060aa1730b1588280baecd106658bf1283
Sherif Nafee <shico.m.nafee@gmail.com>
2024-11-01 14:25:53 +0300

    Update imu to use global and local transforms

--- Runtime/Tx/ImuTx.cs
+++ Runtime/Tx/ImuTx.cs
@@ -9,59 +9,50 @@ namespace ProBridgeSenors.Tx
     [AddComponentMenu("ProBridge/Tx/sensor_msgs/Imu")]
     public class ImuTx : ProBridgeTxStamped<Imu>
     {
-        public Vector3 Acceleration { get; private set; }
+        public Vector3 acceleration { get; private set; }
 
-        public bool isNedImu;
-        private Vector3 _lastVel = new Vector3(0, 0, 0);
-        private Vector3 gravityDirection;
-        private float gravityMagnitude;
-        private Transform initTransform;
+        private Vector3 _lastVel = new(0, 0, 0);
 
-        private Vector3 lastPosition;
-        private Quaternion lastRotation;
-        private Vector3 velocity;
-        private Vector3 angularVelocity;
+        private Vector3 _lastPosition;
+        private Quaternion _lastRotation;
+        private Vector3 _velocity;
+        private Vector3 _angularVelocity;
+        private bool _isGlobal;
 
 
         protected override void OnStart()
         {
-            gravityDirection = Physics.gravity.normalized;
-            gravityMagnitude = Physics.gravity.magnitude;
-            initTransform = transform;
-            lastPosition = transform.position;
-            lastRotation = transform.rotation;
+            _lastPosition = transform.position;
+            _lastRotation = transform.rotation;
         }
 
         void FixedUpdate()
         {
-            velocity = (transform.position - lastPosition) / Time.fixedDeltaTime;
+            _velocity = (transform.position - _lastPosition) / Time.fixedDeltaTime;
 
-            Quaternion deltaRotation = transform.rotation * Quaternion.Inverse(lastRotation);
+            Quaternion deltaRotation = transform.rotation * Quaternion.Inverse(_lastRotation);
             deltaRotation.ToAngleAxis(out var angle, out var axis);
-            angularVelocity = Mathf.Deg2Rad * angle * axis / Time.fixedDeltaTime;
+            _angularVelocity = Mathf.Deg2Rad * angle * axis / Time.fixedDeltaTime;
 
-            lastPosition = transform.position;
-            lastRotation = transform.rotation;
+            _lastPosition = transform.position;
+            _lastRotation = transform.rotation;
 
+            acceleration = (_velocity - _lastVel) / Time.deltaTime;
 
-            if (isNedImu)
+            if (!_isGlobal)
             {
-                Acceleration = (velocity - _lastVel) / Time.deltaTime -
-                               initTransform.InverseTransformDirection(gravityDirection) * gravityMagnitude;
-            }
-            else
-            {
-                Acceleration = (velocity - _lastVel) / Time.deltaTime;
+                acceleration = transform.InverseTransformDirection(acceleration);
+                _angularVelocity = transform.InverseTransformDirection(_angularVelocity);
             }
 
-            _lastVel = velocity;
+            _lastVel = _velocity;
         }
 
 
         protected override ProBridge.ProBridge.Msg GetMsg(TimeSpan ts)
         {
-            data.angular_velocity = (Quaternion.Inverse(transform.rotation) * angularVelocity).ToRosAngular();
-            data.linear_acceleration = Acceleration.ToRos();
+            data.angular_velocity = _angularVelocity.ToRos();
+            data.linear_acceleration = acceleration.ToRos();
             data.orientation = transform.rotation.ToRos();
 
             return base.GetMsg(ts);
